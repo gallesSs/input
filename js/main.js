e4e3b73b00e6f6a6313e1e995fe537425f41d17e
gallesss <agavrik464@gmail.com>
2025-11-23 21:33:54 +0200

    functional added

--- js/main.js
+++ js/main.js
@@ -155,7 +155,6 @@ const imgBtn = document.querySelector(".custom-upload");
 const resultWrapper = document.querySelector('.result--wrapper');
 const btnUrl = document.querySelector(".btn-link");
 
-
 function fileToBase64(file) {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader();
@@ -228,6 +227,9 @@ function updateRightPanelVisibility() {
 		resultWrapper.classList.remove('success');
 		sendBtn.style.display = 'none';
 	}
+	
+	// RESET после любого обновления
+	if (imageInput) imageInput.value = "";
 }
 
 if (imageInput && fileWrapper) {
@@ -245,7 +247,6 @@ function handleFiles(e) {
 	const files = Array.from(e.target.files || []);
 	if (!files.length) return;
 	
-	// === 1️⃣ Проверка лимита (макс. 3 файла)
 	const existingFiles = fileWrapper.querySelectorAll('.file-row').length;
 	if (existingFiles + files.length > 3) {
 		showPopup('limit');
@@ -253,13 +254,11 @@ function handleFiles(e) {
 	}
 	
 	files.forEach(file => {
-		// === 2️⃣ Проверка типа
 		if (!file.type.startsWith('image/')) {
 			showPopup('type');
 			return;
 		}
 		
-		// === 3️⃣ Проверка размера (макс. 5 MB)
 		if (file.size > 5 * 1024 * 1024) {
 			showPopup('size');
 			return;
@@ -279,19 +278,18 @@ function handleFiles(e) {
 		reader.onload = e => img.src = e.target.result;
 		reader.readAsDataURL(file);
 		
-		// === КНОПКА КРОП ===
 		const cropBtn = document.createElement('button');
 		cropBtn.className = 'crop-btn';
-		cropBtn.innerHTML = '<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-			'<g clip-path="url(#clip0_109_17673)">\n' +
-			'<path d="M3.065 0.5L3 8C3 8.26522 3.10536 8.51957 3.29289 8.70711C3.48043 8.89464 3.73478 9 4 9H11.5" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-			'<path d="M0.5 3.065L8 3C8.26522 3 8.51957 3.10536 8.70711 3.29289C8.89464 3.48043 9 3.73478 9 4V11.5" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-			'</g>\n' +
-			'<defs>\n' +
-			'<clipPath id="clip0_109_17673">\n' +
-			'<rect width="12" height="12" fill="white"/>\n' +
-			'</clipPath>\n' +
-			'</defs>\n' +
+		cropBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">\n' +
+			'  <g clip-path="url(#clip0_109_17625)">\n' +
+			'    <path d="M3.065 0.5L3 8C3 8.26522 3.10536 8.51957 3.29289 8.70711C3.48043 8.89464 3.73478 9 4 9H11.5" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+			'    <path d="M0.5 3.065L8 3C8.26522 3 8.51957 3.10536 8.70711 3.29289C8.89464 3.48043 9 3.73478 9 4V11.5" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+			'  </g>\n' +
+			'  <defs>\n' +
+			'    <clipPath id="clip0_109_17625">\n' +
+			'      <rect width="12" height="12" fill="white"/>\n' +
+			'    </clipPath>\n' +
+			'  </defs>\n' +
 			'</svg>';
 		cropBtn.type = 'button';
 		cropBtn.onclick = () => openCropper(img);
@@ -310,12 +308,19 @@ function handleFiles(e) {
 		
 		const remove = document.createElement('button');
 		remove.className = 'remove-btn';
-		remove.textContent = '✖';
+		remove.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">\n' +
+			'  <path d="M15 5L5 15" stroke="#6C6F73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+			'  <path d="M5 5L15 15" stroke="#6C6F73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+			'</svg>`;
 		remove.type = 'button';
 		remove.onclick = () => {
 			row.remove();
 			updatePhotoCount();
 			updateRightPanelVisibility();
+			
+			// RESET при удалении файла
+			if (imageInput) imageInput.value = "";
+			if (cameraInput) cameraInput.value = "";
 		};
 		
 		const progress = document.createElement('div');
@@ -330,6 +335,10 @@ function handleFiles(e) {
 	});
 	
 	updateRightPanelVisibility();
+	
+	// RESET, чтобы можно было выбрать те же файлы
+	if (imageInput) imageInput.value = "";
+	if (cameraInput) cameraInput.value = "";
 }
 
 // === КРОППЕР ===
@@ -353,10 +362,8 @@ function openCropper(imgEl) {
 	
 	zoomInput.value = 1;
 	
-	// zoom
 	zoomInput.oninput = () => cropper.zoomTo(parseFloat(zoomInput.value));
 	
-	// apply
 	cropConfirm.onclick = async () => {
 		const canvas = cropper.getCroppedCanvas({maxWidth: 2000, maxHeight: 2000});
 		const croppedBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
@@ -367,7 +374,6 @@ function openCropper(imgEl) {
 		cropper.destroy();
 	};
 	
-	// cancel & close (оба закрывают без изменений)
 	const closeModal = () => {
 		cropModal.classList.remove('active');
 		cropper.destroy();
@@ -378,6 +384,7 @@ function openCropper(imgEl) {
 
 // ==== ПОСЛЕДОВАТЕЛЬНАЯ ОБРАБОТКА ====
 sendBtn?.addEventListener('click', async () => {
+	
 	const rows = Array.from(fileWrapper.querySelectorAll('.file-row'));
 	if (!rows.length) {
 		alert('Нет файлов для обработки');
@@ -414,28 +421,15 @@ sendBtn?.addEventListener('click', async () => {
 
 // ==== ОБРАБОТКА ОДНОГО ФАЙЛА ====
 async function processSingleFile(file, row, statusEl, progressEl) {
-	/*
-		const uploadProgress = startFakeProgress(progressEl, statusEl, 0, 60);
-	*/
-	
 	let base64;
 	try {
 		base64 = await fileToBase64(file);
-		/*
-				uploadProgress.complete();
-		*/
 	} catch (err) {
-		/*
-				uploadProgress.error();
-		*/
 		statusEl.textContent = 'Ошибка при чтении файла';
 		return 'Ошибка при чтении файла';
 	}
 	
 	statusEl.textContent = 'Processing...';
-	/*
-		const processProgress = startFakeProgress(progressEl, statusEl, 60, 100);
-	*/
 	
 	try {
 		const res = await fetch(API_URL, {
@@ -450,37 +444,38 @@ async function processSingleFile(file, row, statusEl, progressEl) {
 		});
 		
 		const result = await res.json();
-		/*
-				processProgress.complete();
-		*/
 		
 		if (result.success) {
 			statusEl.textContent = 'Done';
 			if (result.text && result.text.trim() !== '') {
-				console.log('OCR result for', file.name, ':', result.text);
 				return result.text;
 			} else {
-				console.log('OCR result for', file.name, ': NO_TEXT_FOUND');
-				return 'NO_TEXT_FOUND';
+				return `
+    <div class="no-text-block">
+        <div class="no-text-top">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M22.3879 18.5627L14.1757 4.0945C13.7756 3.38923 13.028 2.95447 12.2178 2.95447C11.4066 2.95447 10.659 3.38923 10.259 4.0945L2.04761 18.5627C1.65179 19.2591 1.65687 20.1147 2.05988 20.8077C2.4629 21.4998 3.20458 21.9265 4.00595 21.9265H20.4291C21.2309 21.9265 21.9718 21.4998 22.3752 20.8077C22.7786 20.1147 22.7829 19.2587 22.3879 18.5627ZM13.3396 19.268C13.0644 19.5127 12.7376 19.6367 12.3693 19.6367C11.9964 19.6367 11.6666 19.5144 11.388 19.2731C11.1035 19.0259 10.9588 18.6796 10.9588 18.2436C10.9588 17.8609 11.0951 17.5307 11.3639 17.2623C11.6323 16.9943 11.965 16.858 12.3528 16.858C12.7397 16.858 13.075 16.9935 13.3493 17.2614C13.6241 17.5302 13.7638 17.8609 13.7638 18.2436C13.7633 18.6732 13.6207 19.0174 13.3396 19.268ZM13.7041 9.95173L13.3464 14.0487C13.3061 14.5589 13.2198 14.9432 13.0826 15.2226C12.9315 15.5317 12.6678 15.6955 12.3194 15.6955C11.9642 15.6955 11.7026 15.5351 11.5624 15.2315C11.438 14.9619 11.3525 14.5703 11.3004 14.0327L11.0337 10.0457C10.9833 9.25789 10.9583 8.70798 10.9583 8.36466C10.9583 7.86851 11.0934 7.4727 11.3597 7.18822C11.6285 6.9012 11.9853 6.75515 12.4188 6.75515C12.9539 6.75515 13.3197 6.94946 13.5068 7.33215C13.6787 7.68521 13.7625 8.17289 13.7625 8.82271C13.7633 9.191 13.7434 9.57073 13.7041 9.95173Z" fill="#EB5757"/>
+</svg>
+            <span>No Text Found!</span>
+        </div>
+        <div class="no-text-hint">
+            Please upload an image which have visible text.
+        </div>
+    </div>
+`;
 			}
 		} else {
 			const errText = 'Ошибка: ' + (result.error || 'Неизвестная ошибка');
 			statusEl.textContent = 'Error';
-			console.log('OCR result for', file.name, ':', errText);
 			return errText;
 		}
 	} catch (err) {
 		const errText = 'Ошибка: ' + err.message;
-		/*
-				processProgress.error();
-		*/
 		statusEl.textContent = errText;
-		console.log('OCR result for', file.name, ':', errText);
 		return errText;
 	}
 }
 
-// ==== ФИНАЛЬНЫЙ ЭКРАН ====
 // ==== ФИНАЛЬНЫЙ ЭКРАН ====
 function showFinalResults(initialResults) {
 	const heroSection = document.querySelector('.hero');
@@ -502,15 +497,27 @@ function showFinalResults(initialResults) {
 	container.appendChild(header);
 	heroSection.appendChild(container);
 	
-	// создаём карточки для каждой картинки с "фейковой" анимацией
 	initialResults.forEach((res, index) => {
 		const item = document.createElement('div');
 		item.className = 'result-item converting';
 		item.dataset.index = index;
 		item.innerHTML = `
 			<div class="result-img">
+			<button class="info-btn">
+			<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+  <g clip-path="url(#clip0_109_20792)">
+    <path d="M6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11Z" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6 8V6" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6 4H6.005" stroke="#6C6F73" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_109_20792">
+      <rect width="12" height="12" fill="white"/>
+    </clipPath>
+  </defs>
+</svg></button>
 				<img src="${res.imageSrc}" alt="${res.name}">
-				<div class="file-info-line">${res.size}</div>
+				<div class="file-info-line">${res.name}</div>
 			</div>
 			<div class="result-text">
 				<div class="convert-overlay"></div>
@@ -519,7 +526,16 @@ function showFinalResults(initialResults) {
 		`;
 		container.appendChild(item);
 		
-		// фейковая прогресс-анимация
+		const infoBtn = item.querySelector('.info-btn');
+		infoBtn.addEventListener('click', () => {
+			openInfoModal({
+				name: res.name,
+				size: res.size,
+				mimeType: res.mimeType,
+				imageSrc: res.imageSrc
+			});
+		});
+		
 		let progress = 0;
 		const overlay = item.querySelector('.convert-overlay');
 		const progressTimer = setInterval(() => {
@@ -528,39 +544,30 @@ function showFinalResults(initialResults) {
 				progress = 100;
 				clearInterval(progressTimer);
 				
-				// заменяем overlay на финальный результат
 				item.classList.remove('converting');
-				item.querySelector('.result-text').innerHTML = `
-	<textarea>${res.text}</textarea>
-	<div class="item-buttons">
-		<button class="expand-btn"><img src="/images/expand.svg" alt=""></button>
-		<button class="copy-btn"><img src="/images/copy.svg" alt=""></button>
-		<button class="save-btn"><img src="/images/download.svg" alt=""></button>
-	</div>
-`;
+				const resultHTML = res.text.includes('no-text-block')
+					? `${res.text}` // только HTML-блок
+					: `<p contenteditable="true">${res.text}</p>
+       <div class="item-buttons">
+           <button class="expand-btn"><img src="/images/expand.svg" alt=""></button>
+           <button class="copy-btn"><img src="/images/copy.svg" alt=""></button>
+           <button class="save-btn"><img src="/images/download.svg" alt=""></button>
+       </div>`;
 				
-				const textarea = item.querySelector('textarea');
+				item.querySelector('.result-text').innerHTML = resultHTML;
+				
+				
+				const textarea = item.querySelector('p');
 				const expandBtn = item.querySelector('.expand-btn');
 				const copyBtn = item.querySelector('.copy-btn');
 				const saveBtn = item.querySelector('.save-btn');
 				
 				let expanded = false;
+				
 				expandBtn.addEventListener('click', () => {
-					expanded = !expanded;
-					
-					if (expanded) {
-						item.style.maxHeight = '300px'; // или auto, если контент длинный
-						item.style.height = 'auto';
-						textarea.style.height = '100%';
-						textarea.style.marginTop = '25px';
-					} else {
-						item.style.maxHeight = '120px';
-						textarea.style.height = '52px';
-						textarea.style.marginTop = '0';
-					}
-					
-					expandBtn.classList.toggle('active', expanded);
+					item.classList.toggle('expanded');
 				});
+				
 				
 				copyBtn.addEventListener('click', async () => {
 					await navigator.clipboard.writeText(textarea.value);
@@ -576,13 +583,11 @@ function showFinalResults(initialResults) {
 					link.click();
 					URL.revokeObjectURL(link.href);
 				});
-				
 			}
 			overlay.style.setProperty('--progress', `${progress}%`);
 		}, 200);
 	});
 	
-	// кнопки перезапуска и загрузки
 	setTimeout(() => {
 		header.querySelector('h2').textContent = 'Files Converted';
 		header.querySelector('.final-buttons').classList.remove('hidden');
@@ -590,7 +595,6 @@ function showFinalResults(initialResults) {
 		document.querySelector('.download-btn').addEventListener('click', () => downloadZip(initialResults));
 	}, 3500);
 }
-
 
 // === PHOTO MODAL ===
 function openPhotoModal(src) {
@@ -659,15 +663,14 @@ async function downloadZip(results) {
 }
 
 // ==== URL BUTTONS ====
-const btnLink = document.querySelector(".btn-link");
+const btnLinkUrl = document.querySelector(".btn-link");
 const urlBtns = document.querySelector('.url--btns__list');
 const sendBtnUrl = document.getElementById('sendBtnUrl');
 
-btnLink?.addEventListener('click', () => {
+btnLinkUrl?.addEventListener('click', () => {
 	urlBtns?.classList.toggle("active");
 });
 
-// добавляем картинку по URL в тот же список, что и файлы
 sendBtnUrl?.addEventListener('click', () => {
 	addImageFromUrl();
 });
@@ -702,6 +705,11 @@ async function addImageFromUrl() {
 		const file = new File([blob], safeName, {type: blob.type || contentType});
 		
 		handleFiles({target: {files: [file]}});
+		
+		// RESET for URL uploads
+		if (imageInput) imageInput.value = "";
+		input.value = "";
+		
 		updateRightPanelVisibility();
 	} catch (e) {
 		console.error(e);
@@ -720,7 +728,11 @@ cameraBtn?.addEventListener('click', () => {
 cameraInput?.addEventListener('change', (e) => {
 	const files = Array.from(e.target.files || []);
 	if (!files.length) return;
+	
 	handleFiles({target: {files}});
+	
+	// RESET camera input
+	cameraInput.value = "";
 	updateRightPanelVisibility();
 });
 
@@ -748,7 +760,6 @@ function showPopup(type = 'generic') {
 		case 'size':
 			img.src = './images/size.png';
 			title.textContent = 'File Size Exceed';
-			title.textContent = 'File Size Exceed';
 			msg.textContent = 'The website allows users to upload files up to a maximum size of 5 MB only.';
 			break;
 		
@@ -767,7 +778,7 @@ function showPopup(type = 'generic') {
 	};
 }
 
-// ==== DRAG & DROP (вся зона input__container) ====
+// ==== DRAG & DROP ====
 const dropContainer = document.querySelector('.input__container');
 
 if (dropContainer) {
@@ -789,13 +800,18 @@ if (dropContainer) {
 		const files = Array.from(e.dataTransfer.files || []);
 		if (files.length) {
 			handleFiles({target: {files}});
+			
+			// RESET for drag & drop
+			if (imageInput) imageInput.value = "";
+			if (cameraInput) cameraInput.value = "";
+			
 			updateRightPanelVisibility();
 			updatePhotoCount();
 		}
 	});
 }
 
-// ==== CTRL + V (вставка изображений) ====
+// ==== CTRL + V ====
 if (dropContainer) {
 	document.addEventListener('paste', (e) => {
 		const items = e.clipboardData?.items;
@@ -813,24 +829,28 @@ if (dropContainer) {
 		
 		if (files.length) {
 			handleFiles({target: {files}});
+			
+			// RESET for paste
+			if (imageInput) imageInput.value = "";
+			if (cameraInput) cameraInput.value = "";
+			
 			updateRightPanelVisibility();
 			updatePhotoCount();
 		}
 	});
 }
 
-
-// ==== ОБНОВЛЕНИЕ СЧЁТЧИКА ФОТО ====
+// ==== UPDATE PHOTO COUNT ====
 function updatePhotoCount() {
-	const title = document.querySelector('.result-current'); // замени селектор, если у тебя другой
+	const title = document.querySelector('.result-current');
 	if (!title) return;
 	
 	const fileRows = document.querySelectorAll('.file-info-wrapper .file-row');
 	const count = fileRows.length;
 	
 	if (count > 0) {
-		title.textContent = `${count} Photo${count > 1 ? 's' : ''} Uploading`;
+		title.textContent = `${count} Photo${count > 1 ? 's' : ''} Uploaded`;
 	} else {
-		title.textContent = 'Photos Uploading';
+		title.textContent = 'Photos Uploaded';
 	}
 }
